0664989d7cf2235ea0a275a2e854fe6adbc1ee5b
kwangsub <kwangsub.kim@gmail.com>
2013-02-27 11:20:37 +0900

    check style

--- src/test/java/kr/blogspot/kwangsub/SampleServiceTest.java
+++ src/test/java/kr/blogspot/kwangsub/SampleServiceTest.java
@@ -19,8 +19,6 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.transaction.TransactionConfiguration;
-import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:applicationContext-test.xml"})
